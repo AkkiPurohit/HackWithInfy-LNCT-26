d21b41afca2d982372925638c679b978a793c849
Alankrit Purohit <purohitak21@gmail.com>
2026-04-05 21:03:22 +0530

    Add files via upload

---
+++ remove_duplicate_in_string_while_reserving_min_lexicographical_order.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeDuplicateLetters(string s) {
+    vector<int> lastOccurence(26, -1);
+    vector<bool> taken(26, false);
+    string result = "";
+
+    for (int i = 0; i < s.length(); i++) {
+        lastOccurence[s[i] - 'a'] = i;
+    }
+
+    for (int i = 0; i < s.length(); i++) {
+        char ch = s[i];
+        int idx = ch - 'a';
+
+        if (taken[idx]) continue;
+
+        while (!result.empty() &&
+               result.back() > ch &&
+               lastOccurence[result.back() - 'a'] > i) {
+            
+            taken[result.back() - 'a'] = false;
+            result.pop_back();
+        }
+
+        result.push_back(ch);
+        taken[idx] = true;
+    }
+
+    return result;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    cout << removeDuplicateLetters(s) << endl;
+
+    return 0;
+}
